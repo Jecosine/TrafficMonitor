2353e46caf36bba27c7247dc412b36a97c8b2801
workspace <mj_ol@outlook.com>
2019-10-02 16:47:32 +0800

    UPDATE:Update get_args function

--- loop_record.py
+++ loop_record.py
@@ -2,4 +2,20 @@ import cv2
 import sys
 
 # read sysetm args
-
+# usage: loop_record.py [dev] [fps] [time(s)] [output]
+def get_args():
+    args = sys.argv[1:]
+    if len(args) <> 4:
+        print("usage : loop_record.py [dev] [fps] [time(s)] [output]")
+        return False
+    else:
+        dev, fps, t, output = args
+        try:
+            dev = int(dev)
+            fps = int(fps)
+            time = fps * int(time)
+        except Exception as e:
+            print("Invalid arg occurs error {0}".format(e))
+            return False
+        else:
+            return True
